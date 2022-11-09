1189e2bbb02ce12496e709850de51afe94d35b22
MJ-Curtido <manu17pc@gmail.com>
2022-11-09 14:42:38 +0100

    no se porque no va

--- src/Servidor/Servidor.java
+++ src/Servidor/Servidor.java
@@ -5,24 +5,28 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Iterator;
 
 public class Servidor {
 
 	public static void main(String[] args) throws IOException {
-		int puerto = 6007;
+		int puerto = 6070;
 		
 		ServerSocket servidor = new ServerSocket(puerto);
 		
-		System.out.println("Escuchando en " + servidor.getLocalPort());
-		
-		Socket cliente = servidor.accept();
-		
-		DataInputStream msgCliente = new DataInputStream(cliente.getInputStream());
-		System.out.println(msgCliente.readUTF());
-		
-		DataOutputStream msgServidor = new DataOutputStream(cliente.getOutputStream());
-		msgServidor.writeUTF("Saludos al cliente del servidor");
-		
-		servidor.close();
+		for (int i = 1; ; i++) {
+			System.out.println("Escuchando en " + servidor.getLocalPort());
+			
+			Socket cliente = servidor.accept();
+			
+			DataInputStream msgCliente = new DataInputStream(cliente.getInputStream());
+			System.out.println("Cliente " + i + ": " + msgCliente.readUTF());
+			
+			DataOutputStream msgServidor = new DataOutputStream(cliente.getOutputStream());
+			msgServidor.writeUTF("Saludos al cliente del servidor");
+			
+			msgCliente.close();
+			msgServidor.close();
+		}
 	}
 }
